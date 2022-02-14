16595b44bc6fede502a3d612a8f240e4130e7f71
TophAnomandris <92409305+TophAnomandris@users.noreply.github.com>
2022-02-14 15:32:52 +0100

    The game currently works and we can hold dices now.
    there's currently no code to inform the players if they've won

--- tenzies-app/src/App.js
+++ tenzies-app/src/App.js
@@ -5,29 +5,51 @@ import {nanoid} from 'nanoid'
 export default function App() {
 
 
-  const[dieArray, newDieArray]= React.useState(allNewDice())
+const[dieArray, newDieArray]= React.useState(allNewDice())
 
-function rollNewDie(){
-newDieArray(allNewDice())
+
+function generateNewDie(){
+    const roll = Math.ceil(Math.random() * 6)
+    return{value: roll, 
+        isHeld: false,
+        id: nanoid()}
 }
-
-  function allNewDice(){
+function allNewDice(){
     const array = []
     for(let i = 0; i<10; i++){
-      var roll = Math.ceil(Math.random() * 6)
-      array.push({value: roll, 
-                  isHeld: true,
-                  id: nanoid()})
-  }
-  return array}
-  console.log(allNewDice())
+      array.push(generateNewDie())}                  
+     return array}
+
+function rollNewDie(){
+newDieArray(prevArray=> prevArray.map(die => {
+    if(die.isHeld === true){
+      return die
+    }
+    else{ return generateNewDie()}
+}))}
+
+
+function holdDice(id){
+    newDieArray(prevArray =>prevArray.map(die => { 
+
+    return die.id === id? {...die, isHeld: !die.isHeld} : die
+    }))
+
+    }
+  console.log(dieArray)
   
-    const diceElements = dieArray.map(die=> <Die className="face" key={die.id} value={die.value}/>)
+  const diceElements = dieArray.map(die=> <Die 
+  handleClick={()=> holdDice(die.id)}
+  key={die.id} 
+  value={die.value}
+  isHeld={die.isHeld}/>)
   
   
 
   return (
     <main className='wrapper'>
+      <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className='parent'>
           {diceElements}
       </div>
--- tenzies-app/src/components/Dice.js
+++ tenzies-app/src/components/Dice.js
@@ -5,8 +5,12 @@ import React from "react";
 
 export default function Die(props){
 
+
+    const styles = {
+        background: props.isHeld ?  '#59E391' :  '#FFFFFF'}
+
     return(
-        <div className="face">
+        <div className="face" style={styles} onClick={props.handleClick}>
             <h2 className="die--number">{props.value}</h2>
         </div>
     )
